85104fded988f132d7715b10a1336103666b759e
Matt Cook <mcc1985@gmail.com>
2019-02-11 16:19:53 +0000

    used get to aquire data from url and printed to terminal in chunks

---
+++ printing-chunks/printing-chunks.js
@@ -0,0 +1,23 @@
+const https = require('https');
+
+
+function getAndPrintHTMLChunks() {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html',
+  };
+
+   //Add your code here
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+    response.on('data', function (data) {
+      console.log(`${data}\n`);
+    });
+    response.on('end', function () {
+      console.log(`Response stream complete.`)
+    });
+  });
+}
+
+getAndPrintHTMLChunks();
