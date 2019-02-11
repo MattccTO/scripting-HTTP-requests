4e36e76430d77c848d7c67db6c8220598c20113b
Matt Cook <mcc1985@gmail.com>
2019-02-11 17:58:18 +0000

    Completed print-lowercase.js which uses getHTML module and prints data in lowercase

--- printing-http-request/callbacks/print-lowercase.js
+++ printing-http-request/callbacks/print-lowercase.js
@@ -1,1 +1,12 @@
 const getHTML = require('../HTTP-functions');
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/lowercase.html',
+};
+
+function printLowercase(html) {
+  console.log(html.toLowerCase());
+}
+
+getHTML(requestOptions, printLowercase);
