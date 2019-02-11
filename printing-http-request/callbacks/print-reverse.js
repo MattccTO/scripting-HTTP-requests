a0e77ab2738ff4ad312308167aa63cd6f8510b24
Matt Cook <mcc1985@gmail.com>
2019-02-11 18:02:16 +0000

    Add print-reverse.js which uses getHTML module and prints data in reverse

--- printing-http-request/callbacks/print-reverse.js
+++ printing-http-request/callbacks/print-reverse.js
@@ -2,11 +2,11 @@ const getHTML = require('../HTTP-functions');
 
 const requestOptions = {
   host: 'sytantris.github.io',
-  path: '/http-examples/step6/lowercase.html',
+  path: '/http-examples/step6/reverse.html',
 };
 
-function printLowercase(html) {
-  console.log(html.toLowerCase());
+function printReverse(html) {
+  console.log(html.split('').reverse().join(''));
 }
 
-getHTML(requestOptions, printLowercase);
+getHTML(requestOptions, printReverse);
